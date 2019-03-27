344ccc9d353d2ea8da0563cb9d31265a140e69e6
Tim Allen <tnelsallen3@gmail.com>
2019-03-27 08:43:08 -0600

    Added vcr senstive info filter

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,7 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
+  config.filter_sensitive_data('<NREL_KEY>') { ENV['NREL_KEY'] }
 end
 
 Shoulda::Matchers.configure do |config|
