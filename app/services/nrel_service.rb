11b4089c4acb6bf54f8151df55ed561c31e4d5d7
Tim Allen <tnelsallen3@gmail.com>
2019-03-27 10:13:48 -0600

    Was reading the api docs and added uri to as a parameter to yet unmade get_json method

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,5 +1,5 @@
 class NrelService
-  def get_results(quantity = 0)
-    get_json()
+  def get_results(limit = 0)
+    get_json("/api/alt-fuel-stations/v1.json?limit=#{limit}&api_key=#{ENV['NREL_KEY']}")
   end
 end
